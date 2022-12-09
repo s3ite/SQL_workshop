73459762cd79fb6615c1fa02cc2764717e4652f9
s3iteGustave <guy-noe.djoufaing-nzumgueng@epita.fr>
2022-12-09 19:19:53 +0100

    fix: music level 0 views

--- music_renting_company/level_0/views.sql
+++ music_renting_company/level_0/views.sql
@@ -9,7 +9,7 @@ CREATE OR REPLACE VIEW view_albums
 AS
 Select  album.name as album, count(art_id) as songs, (select duration_to_string(duration)) as duration 
 FROM album
-JOIN music_album ON music_album.alb_id = album.id
+LEFT JOIN music_album ON music_album.alb_id = album.id
 JOIN music ON music.id = music_album.music_id
 GROUP BY album, duration
 ORDER BY album;
