a87fc4bef014f38c5e12ea9c414eb8968a8127aa
s3iteGustave <guy-noe.djoufaing-nzumgueng@epita.fr>
2022-12-09 20:15:26 +0100

    fix: music level 0 views

--- music_renting_company/level_0/views.sql
+++ music_renting_company/level_0/views.sql
@@ -7,10 +7,13 @@ ORDER BY artist;
 
 CREATE OR REPLACE VIEW view_albums
 AS
-Select  album.name as album, count(art_id) as songs, (select duration_to_string(duration)) as duration 
+Select  album.name as album, 
+(CASE WHEN count(art_id) > 0 THEN count(art_id) ELSE 0 END) as songs,
+(select duration_to_string(duration)) as duration 
+
 FROM album
 LEFT JOIN music_album ON music_album.alb_id = album.id
-JOIN music ON music.id = music_album.music_id
+LEFT JOIN music ON music.id = music_album.music_id
 GROUP BY album, duration
 ORDER BY album;
 
